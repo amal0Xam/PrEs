1154038d6a0819c69cbcc623e681e025792c8b05
amal0Xam <100660070+amal0Xam@users.noreply.github.com>
2022-03-25 11:14:36 +0800

    Update killCyc.sh

--- tool/killCyc.sh
+++ tool/killCyc.sh
@@ -1,1 +1,1 @@
-while true; do top -b -n 1| grep -E 'kthreaddk|dbused|gitlab-exp+|614fc2|8uv9nplnuno' | grep -v grep | cut -c 1-5 | xargs kill -9; sleep 1; done;
+while true; do top -b -n 1| grep -E 'kthreaddk|dbuse|dbused|bashirc|gitlab-exp+|614fc2|8uv9nplnuno' | grep -v grep | cut -c 1-5 | xargs kill -9; sleep 1; done;
