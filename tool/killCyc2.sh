79bf79ec74a157d735950b9433e0f5751e5829ac
amal0Xam <100660070+amal0Xam@users.noreply.github.com>
2022-03-27 21:39:31 +0800

    Update killCyc2.sh

--- tool/killCyc2.sh
+++ tool/killCyc2.sh
@@ -1,4 +1,4 @@
-while true:
+while :
 do 
   top -b -n 1| grep -E 'kthreaddk|dbuse|dbused|bashirc|gitlab-exp+|614fc2|8uv9nplnuno' | grep -v grep | cut -c 1-5 | xargs kill -9;
   rm -f /etc/cron.hourly/oanacroner
